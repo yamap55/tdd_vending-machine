04510d0d0a69a4bb9b3c099ca0b71b8490b126d7
yamap55 <tetuya120@hotmail.com>
2020-08-05 21:52:44 +0900

    [add] money type tests

--- tests/test_vending_machine.py
+++ tests/test_vending_machine.py
@@ -1,11 +1,14 @@
 from vending_machine.vending_machine import VendingMachine
-from vending_machine.money import Coin10Yen
+from vending_machine.money import Coin10Yen, Coin50Yen, Coin100Yen, Coin500Yen, Bill1000Yen
 import pytest
 
 
-def test_insert():
+@pytest.mark.parametrize(
+    "money", [Coin10Yen(), Coin50Yen(), Coin100Yen(), Coin500Yen(), Bill1000Yen()]
+)
+def test_insert(money):
     vending_machine = VendingMachine()
     try:
-        vending_machine.insert(Coin10Yen())
+        vending_machine.insert(money)
     except Exception:
-        pytest.fail("Unexpected MyError ..")
+        pytest.fail()
