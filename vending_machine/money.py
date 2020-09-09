393b183535d0f2dea8388772d94c1d71321f22b4
yamap55 <tetuya120@hotmail.com>
2020-09-09 18:31:15 +0900

    [add] money

---
+++ vending_machine/money.py
@@ -0,0 +1,51 @@
+"""お金"""
+
+
+class Money:
+    """お金"""
+
+    pass
+
+
+class Bill(Money):
+    """お札"""
+
+    pass
+
+
+class Bill1000(Bill):
+    """1000円札"""
+
+    pass
+
+
+class Coin(Money):
+    """硬貨"""
+
+    # 硬貨をインスタンス化して良いのか検討
+
+    pass
+
+
+class Coin10(Coin):
+    """100円硬貨"""
+
+    pass
+
+
+class Coin50(Coin):
+    """100円硬貨"""
+
+    pass
+
+
+class Coin100(Coin):
+    """100円硬貨"""
+
+    pass
+
+
+class Coin500(Coin):
+    """100円硬貨"""
+
+    pass
