c3183386287790fac2d18fac2a07c79015997b78
KoseiHamada <kosei_hamada@albert2005.co.jp>
2020-09-23 18:08:32 +0900

    fixed type hint

--- vending_machine/vending_machine.py
+++ vending_machine/vending_machine.py
@@ -1,13 +1,12 @@
 """自動販売機"""
 
 from .money import Money
-from typing import List
 
 
 class VendingMachine:
     """自動販売機"""
 
-    def insert(self, *money: List[Money]) -> None:
+    def insert(self, *money: Money) -> None:
         """
         お金投入
 
