751b3605f45bba7b6ad5fee2572f9a9e80cba6b7
yamap55 <tetuya120@hotmail.com>
2020-09-23 18:37:27 +0900

    [update] Implement get_total_amount

--- vending_machine/vending_machine.py
+++ vending_machine/vending_machine.py
@@ -6,6 +6,9 @@ from .money import Money
 class VendingMachine:
     """自動販売機"""
 
+    def __init__(self, amount = 0) -> None:
+        self.amount = amount
+
     def insert(self, *money: Money) -> None:
         """
         お金投入
@@ -16,3 +19,6 @@ class VendingMachine:
             投入するお金
         """
         pass
+
+    def get_total_amount(self) -> int:
+        return self.amount
