05bf3b314557b5a31dad2a9b6a50abb67d5f8d0d
yamap55 <tetuya120@hotmail.com>
2020-09-30 18:44:49 +0900

    [add] coin_box

--- tests/test_vending_machine.py
+++ tests/test_vending_machine.py
@@ -4,29 +4,28 @@ import pytest
 
 
 class TestInsert:
-    @pytest.mark.parametrize(
-        "money, money_yen", [(Coin10(), 10), (Coin50(), 50), (Coin100(), 100), (Bill1000(), 1000)]
-    )
-    def test_insert_money(self, money, money_yen):
+    @pytest.mark.parametrize("money", [Coin10(), Coin50(), Coin100(), Bill1000()])
+    def test_insert_money(self, money):
         vending_machine = VendingMachine()
         vending_machine.insert(money)
 
-        actual = vending_machine.amount
-        expected = money_yen
+        actual = vending_machine.money_box
+        expected = [money]
         assert actual == expected
 
     def test_insert_multiple_money(self):
         vending_machine = VendingMachine()
-        vending_machine.insert(Coin10(), Bill1000())
+        coin10, bill1000 = Coin10(), Bill1000()
+        vending_machine.insert(coin10, bill1000)
 
-        actual = vending_machine.amount
-        expected = 1010
+        actual = vending_machine.money_box
+        expected = [coin10, bill1000]
         assert actual == expected
 
 
 class TestGetTotalAmount:
     def test_amount_10(self):
-        vending_machine = VendingMachine(amount=10)
+        vending_machine = VendingMachine(money_box=[Coin10()])
         actual = vending_machine.get_total_amount()
         expected = 10
         assert actual == expected
--- vending_machine/vending_machine.py
+++ vending_machine/vending_machine.py
@@ -1,14 +1,15 @@
 """自動販売機"""
 
 from .money import Money
+from typing import List
 
 
 class VendingMachine:
     """自動販売機"""
 
-    def __init__(self, amount=0) -> None:
+    def __init__(self, money_box: List = None) -> None:
         """初期化処理"""
-        self.amount = amount
+        self.money_box = money_box if money_box else []
 
     def insert(self, *moneys: Money) -> None:
         """
@@ -20,8 +21,11 @@ class VendingMachine:
             投入するお金
         """
         for money in moneys:
-            self.amount += money.yen
+            self.money_box.append(money)
 
     def get_total_amount(self) -> int:
         """投入金額の総計を取得"""
-        return self.amount
+        total = 0
+        for money in self.money_box:
+            total += money.yen
+        return total
