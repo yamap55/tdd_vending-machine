f45fd1c7a755df9dd8d6ae2ad8628e09ea35ff10
yamap55 <tetuya120@hotmail.com>
2020-09-30 18:16:56 +0900

    [add] comment

--- vending_machine/vending_machine.py
+++ vending_machine/vending_machine.py
@@ -6,7 +6,8 @@ from .money import Money
 class VendingMachine:
     """自動販売機"""
 
-    def __init__(self, amount = 0) -> None:
+    def __init__(self, amount=0) -> None:
+        """初期化処理"""
         self.amount = amount
 
     def insert(self, *moneys: Money) -> None:
@@ -22,4 +23,5 @@ class VendingMachine:
             self.amount += money.yen
 
     def get_total_amount(self) -> int:
+        """投入金額の総計を取得"""
         return self.amount
