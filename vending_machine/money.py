e24492437ed6ac4091a42fbce5f84954b9728368
yamap55 <tetuya120@hotmail.com>
2021-01-21 19:02:06 +0900

    [add] money

--- vending_machine/money.py
+++ vending_machine/money.py
@@ -2,8 +2,19 @@
 お金
 """
 
+
 class Money:
     """
     お金
     """
-    pass
+
+    def __init__(self, price: int):
+        """
+        コンストラクタ
+
+        Parameters
+        ----------
+        price : int
+            価値 # TODO: 命名
+        """
+        self.price = price
