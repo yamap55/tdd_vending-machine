c2e57e5e1ef8ca29b28ab5ff8cd2a7573a0ab2d7
AtsushiOzawa <atsushi_ozawa@albert2005.co.jp>
2021-02-05 10:51:35 +0900

    [Add] insert to vending machine

--- tests/vending_machine/test_vending_machine.py
+++ tests/vending_machine/test_vending_machine.py
@@ -1,6 +1,17 @@
+import pytest
+
+from vending_machine.money import Money
 from vending_machine.vending_machine import VendingMachine
 
 
 def test_exists_vending_machine():
 
     assert VendingMachine()
+
+
+def test_insert():
+    money = Money(100)
+    try:
+        VendingMachine().insert(money)
+    except AttributeError:
+        pytest.fail()
--- vending_machine/vending_machine.py
+++ vending_machine/vending_machine.py
@@ -2,10 +2,21 @@
 飲み物の自動販売機
 """
 
+from vending_machine.money import Money
+
 
 class VendingMachine:
     """
     飲み物の自動販売機
     """
 
-    pass
+    def insert(self, money: Money):
+        """
+        お金を投入。
+
+        Parameters
+        ----------
+        money : Money
+            投入金額
+        """
+        pass
