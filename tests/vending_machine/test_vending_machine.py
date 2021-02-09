3c59980302d7ea01ee13a302be45a2956c3354f0
yamap55 <tetuya120@hotmail.com>
2021-02-09 09:39:19 +0900

    [add] total attribute unit test

--- tests/vending_machine/test_vending_machine.py
+++ tests/vending_machine/test_vending_machine.py
@@ -35,3 +35,16 @@ def test_multiple_insert():
         VendingMachine().insert(money1, money2)
     except AttributeError:
         pytest.fail()
+
+
+def test_total():
+    money1 = Money(10)
+    money2 = Money(100)
+
+    vending_machine = VendingMachine()
+    vending_machine.insert(money1, money2)
+
+    actual = vending_machine.total
+    expected = 110
+
+    assert actual == expected
