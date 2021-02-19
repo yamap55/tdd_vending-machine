3a839f2e0f0878560c4e98cc6d7bd34ccfe72655
AtsushiOzawa <atsushi_ozawa@albert2005.co.jp>
2021-02-19 19:31:04 +0900

    [refactor] test_insert

--- tests/vending_machine/test_vending_machine.py
+++ tests/vending_machine/test_vending_machine.py
@@ -10,15 +10,15 @@ def test_exists_vending_machine():
 
 
 @pytest.mark.parametrize(
-    "amount",
-    [10, 50, 100, 500, 1000],
+    "amount_list,expected_total",
+    [([], 0), ([10], 10), ([10, 50, 100, 500, 1000], 1660)]
+    # [10, 50, 100, 500, 1000]
 )
-def test_insert(amount):
-    money = Money(amount)
-    try:
-        VendingMachine().insert(money)
-    except AttributeError:
-        pytest.fail()
+def test_insert(amount_list, expected_total):
+    money_list = [Money(amount) for amount in amount_list]
+    vending_machine = VendingMachine()
+    vending_machine.insert(*money_list)
+    assert vending_machine.total == expected_total
 
 
 def test_insert_10():
