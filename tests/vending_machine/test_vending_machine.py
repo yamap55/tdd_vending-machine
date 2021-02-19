a6677c9f68777ea72b07f1747d4ada171fe97c86
AtsushiOzawa <atsushi_ozawa@albert2005.co.jp>
2021-02-19 19:31:58 +0900

    [refactor] add space

--- tests/vending_machine/test_vending_machine.py
+++ tests/vending_machine/test_vending_machine.py
@@ -10,55 +10,10 @@ def test_exists_vending_machine():
 
 
 @pytest.mark.parametrize(
-    "amount_list,expected_total",
-    [([], 0), ([10], 10), ([10, 50, 100, 500, 1000], 1660)]
-    # [10, 50, 100, 500, 1000]
+    "amount_list, expected_total", [([], 0), ([10], 10), ([10, 50, 100, 500, 1000], 1660)]
 )
 def test_insert(amount_list, expected_total):
     money_list = [Money(amount) for amount in amount_list]
     vending_machine = VendingMachine()
     vending_machine.insert(*money_list)
     assert vending_machine.total == expected_total
-
-
-def test_insert_10():
-    money = Money(10)
-    try:
-        VendingMachine().insert(money)
-    except AttributeError:
-        pytest.fail()
-
-
-def test_multiple_insert():
-    money1 = Money(10)
-    money2 = Money(100)
-
-    try:
-        VendingMachine().insert(money1, money2)
-    except AttributeError:
-        pytest.fail()
-
-
-def test_total():
-    money1 = Money(10)
-    money2 = Money(100)
-
-    vending_machine = VendingMachine()
-    vending_machine.insert(money1, money2)
-
-    actual = vending_machine.total
-    expected = 110
-
-    assert actual == expected
-
-
-def test_total_1():
-    money1 = Money(10)
-
-    vending_machine = VendingMachine()
-    vending_machine.insert(money1)
-
-    actual = vending_machine.total
-    expected = 10
-
-    assert actual == expected
