ced4fb9296cd9fbb5dd3ee50aeb3bc960f512aea
yamap55 <tetuya120@hotmail.com>
2021-03-03 19:58:37 +0900

    [refactoring] unitest classified

--- tests/vending_machine/test_vending_machine.py
+++ tests/vending_machine/test_vending_machine.py
@@ -4,26 +4,27 @@ from vending_machine.money import Money
 from vending_machine.vending_machine import VendingMachine
 
 
-def test_exists_vending_machine():
+class TestVendingMachine:
+    @pytest.fixture(autouse=True)
+    def setup(self):
+        self.vending_machine = VendingMachine()
 
-    assert VendingMachine()
+    def test_exists_vending_machine(self):
+        assert self.vending_machine
 
+    @pytest.mark.parametrize(
+        "amount_list, expected_total", [([], 0), ([10], 10), ([10, 50, 100, 500, 1000], 1660)]
+    )
+    def test_insert(self, amount_list, expected_total):
+        money_list = [Money(amount) for amount in amount_list]
 
-@pytest.mark.parametrize(
-    "amount_list, expected_total", [([], 0), ([10], 10), ([10, 50, 100, 500, 1000], 1660)]
-)
-def test_insert(amount_list, expected_total):
-    money_list = [Money(amount) for amount in amount_list]
-    vending_machine = VendingMachine()
-    vending_machine.insert(*money_list)
-    assert vending_machine.total == expected_total
+        self.vending_machine.insert(*money_list)
+        assert self.vending_machine.total == expected_total
 
+    @pytest.mark.parametrize("money_list", [[Money(10)], [Money(10), Money(1000)]])
+    def test_pay_back(self, money_list):
+        self.vending_machine.insert(*money_list)
 
-@pytest.mark.parametrize("money_list", [[Money(10)], [Money(10), Money(1000)]])
-def test_pay_back(money_list):
-    vending_machine = VendingMachine()
-    vending_machine.insert(*money_list)
-
-    actual = vending_machine.pay_back()
-    expected = money_list
-    assert actual == expected
+        actual = self.vending_machine.pay_back()
+        expected = money_list
+        assert actual == expected
