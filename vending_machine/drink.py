fc52f073b4ccff0cf956464a74029b4e359ac057
yamap55 <tetuya120@hotmail.com>
2021-04-23 09:49:03 +0900

    [add] drink

---
+++ vending_machine/drink.py
@@ -0,0 +1,36 @@
+"""
+飲み物
+"""
+from abc import ABC
+
+
+class Drink(ABC):
+    """
+    飲み物
+    """
+
+    pass
+
+
+class Cola(Drink):
+    """
+    コーラ
+    """
+
+    pass
+
+
+class Water(Drink):
+    """
+    水
+    """
+
+    pass
+
+
+class Tea(Drink):
+    """
+    お茶
+    """
+
+    pass
